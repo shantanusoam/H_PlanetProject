84b7810748b0e1ed64c76345cb9c09b4cf1cdd74
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-12-05 19:02:26 +0530

    loc

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const { loadPlanetsData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
 const MONGO_URL =
-  'mongodb+srv://Testing-Learn:03gHyb6m1b8TVUEd@cluster0.rbdkzap.mongodb.net/isro?retryWrites=true&w=majority';
+  'mongodb+srv://ISRO-API:88aDKUFdpXH9NmI9@cluster0.nbrazer.mongodb.net/?retryWrites=true&w=majority';
 const server = http.createServer(app);
 
 mongoose.connection.once('open', () => {
